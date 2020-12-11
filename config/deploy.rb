cf9942c6032246326baf54a684b17f9570a5c939
Moosehead <embaquie@gmail.com>
2020-12-11 12:58:36 -0500

    capistrano files

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,8 @@ lock "~> 3.14.1"
 
 set :application, "secondcloset-wharehouse-api"
 set :repo_url, "git@github.com:Moosehead/secondcloset-wharehouse-api.git"
+set :user,'deploy'
+
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
